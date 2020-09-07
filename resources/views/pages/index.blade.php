8b742b1f4b14fa0a430c708bc2bbe44bdb78e6c0
gonela0526 <gonela.sumanth@gmail.com>
2020-09-07 10:33:56 +0530

    commit message

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+  @extends('layouts.app')
+
+  @section('content')
+
+  <div class="jumbotron text-center">
+    <h1>Welcome to Laravel</h1>
+    <p>This is a laravel Application</p>
+    <p><a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">Login</a> <a class="btn btn-success btn-lg" href="{{ route('register') }}" role="button">Register</a></p>
+  </div>
+  @endsection
+
+
+        
