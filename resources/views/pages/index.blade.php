ccc0f881dde5ad1ebc2c88a52dde2f47019f56e6
gonela0526 <gonela.sumanth@gmail.com>
2020-09-07 11:03:22 +0530

    commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,6 +8,3 @@
     <p><a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">Login</a> <a class="btn btn-success btn-lg" href="{{ route('register') }}" role="button">Register</a></p>
   </div>
   @endsection
-
-
-        
